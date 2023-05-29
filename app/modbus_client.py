e63d17dc7631d7b57f305b1e69027c79fc91d6f0
Sebastian Machuca <sebastian.machuca@evergen.energy>
2023-05-29 12:50:21 +1000

    Flagging the client as private

--- app/modbus_client.py
+++ app/modbus_client.py
@@ -3,24 +3,26 @@ from app.configuration import Configuration
 
 
 class ModbusClient:
+    _client: ModbusTcpClient
+
     def __init__(self, configuration: Configuration, port: int, host: str) -> None:
         self.configuration = configuration
-        self.client = ModbusTcpClient(host, port=port)
+        self._client = ModbusTcpClient(host, port=port)
 
     def write_coils(self, name: str, value: list[bool]):
         coil_configuration = self.configuration.get_coil(name)
-        self.client.connect()
-        self.client.write_coils(coil_configuration.address[0], value)
-        self.client.close()
+        self._client.connect()
+        self._client.write_coils(coil_configuration.address[0], value)
+        self._client.close()
 
     def write_coil(self, name: str, value: bool):
         coil_configuration = self.configuration.get_coil(name)
-        self.client.connect()
-        self.client.write_coil(coil_configuration.address[0], value)
-        self.client.close()
+        self._client.connect()
+        self._client.write_coil(coil_configuration.address[0], value)
+        self._client.close()
 
     def write_register(self, name: str, value):
         holding_register_configuration = self.configuration.get_holding_register(name)
-        self.client.connect()
-        self.client.write_register(holding_register_configuration.address[0], value)
-        self.client.close()
+        self._client.connect()
+        self._client.write_register(holding_register_configuration.address[0], value)
+        self._client.close()
