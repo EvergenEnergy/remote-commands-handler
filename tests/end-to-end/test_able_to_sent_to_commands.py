3f157e80c51609aa297fc6f5572c49b8e838aad8
Stijn Bijnen <stijn.bijnen@evergen.energy>
2023-06-06 13:43:55 +0200

    ran black formatter

--- tests/end-to-end/test_able_to_sent_to_commands.py
+++ tests/end-to-end/test_able_to_sent_to_commands.py
@@ -9,14 +9,12 @@ from pymodbus.client import ModbusTcpClient
 from pymodbus.payload import BinaryPayloadDecoder
 from pymodbus.constants import Endian
 
+
 @pytest.mark.end_to_end
 def test_able_to_update_coil():
     random_value = random.randint(1, 2) % 2 == 0
 
-    message_dictionary = {
-        "action": "testRegister",
-        "value": random_value
-    }
+    message_dictionary = {"action": "testRegister", "value": random_value}
     message = json.dumps(message_dictionary)
 
     mqttClient = mqtt.Client()
@@ -24,7 +22,6 @@ def test_able_to_update_coil():
         "localhost",
         5020,
     )
-
 
     modbusClient.connect()
     mqttClient.connect("localhost", 1883, 60)
@@ -39,13 +36,11 @@ def test_able_to_update_coil():
     modbusClient.close()
     mqttClient.disconnect()
 
-@pytest.mark.end_to_end    
+
+@pytest.mark.end_to_end
 def test_able_to_send_integer16():
     random_value = random.randint(0, 30)
-    message_dictorionary = {
-        "action": "testRegister",
-        "value": random_value
-    }
+    message_dictorionary = {"action": "testRegister", "value": random_value}
 
     message = json.dumps(message_dictorionary)
 
@@ -54,7 +49,6 @@ def test_able_to_send_integer16():
         "localhost",
         5020,
     )
-
 
     modbusClient.connect()
     mqttClient.connect("localhost", 1883, 60)
@@ -69,13 +63,11 @@ def test_able_to_send_integer16():
     modbusClient.close()
     mqttClient.disconnect()
 
+
 @pytest.mark.end_to_end
 def test_able_to_send_float32():
     random_value = random.uniform(0, 30)
-    message_dictionary = {
-        "action": "testFloatRegister",
-        "value": random_value
-    }
+    message_dictionary = {"action": "testFloatRegister", "value": random_value}
 
     message = json.dumps(message_dictionary)
 
@@ -94,19 +86,19 @@ def test_able_to_send_float32():
 
     value = modbusClient.read_holding_registers(2, 4, 1)
 
-    decoder = BinaryPayloadDecoder.fromRegisters(value.registers, Endian.Big, wordorder=Endian.Big)
+    decoder = BinaryPayloadDecoder.fromRegisters(
+        value.registers, Endian.Big, wordorder=Endian.Big
+    )
 
     assert round(decoder.decode_32bit_float(), 4) == round(random_value, 4)
     modbusClient.close()
     mqttClient.disconnect()
 
+
 @pytest.mark.end_to_end
 def test_able_to_send_command_with_float64():
     random_value = random.uniform(0, 30)
-    message_dictionary = {
-        "action": "testFloat64Register",
-        "value": random_value
-    }
+    message_dictionary = {"action": "testFloat64Register", "value": random_value}
 
     message = json.dumps(message_dictionary)
 
@@ -115,7 +107,6 @@ def test_able_to_send_command_with_float64():
         "localhost",
         5020,
     )
-
 
     modbusClient.connect()
     mqttClient.connect("localhost", 1883, 60)
@@ -126,18 +117,18 @@ def test_able_to_send_command_with_float64():
 
     value = modbusClient.read_holding_registers(3, 8, 1)
 
-    decoder = BinaryPayloadDecoder.fromRegisters(value.registers, Endian.Big, wordorder=Endian.Big)
+    decoder = BinaryPayloadDecoder.fromRegisters(
+        value.registers, Endian.Big, wordorder=Endian.Big
+    )
     assert decoder.decode_64bit_float() == random_value
     modbusClient.close()
     mqttClient.disconnect()
 
+
 @pytest.mark.end_to_end
 def test_able_to_send_integer64():
     random_value = random.randint(0, 100)
-    message_dictorionary = {
-        "action": "testInt64Register",
-        "value": random_value
-    }
+    message_dictorionary = {"action": "testInt64Register", "value": random_value}
 
     message = json.dumps(message_dictorionary)
 
@@ -146,7 +137,6 @@ def test_able_to_send_integer64():
         "localhost",
         5020,
     )
-
 
     modbusClient.connect()
     mqttClient.connect("localhost", 1883, 60)
@@ -157,19 +147,19 @@ def test_able_to_send_integer64():
 
     value = modbusClient.read_holding_registers(20, 8, 1)
 
-    decoder = BinaryPayloadDecoder.fromRegisters(value.registers, Endian.Big, wordorder=Endian.Big)
+    decoder = BinaryPayloadDecoder.fromRegisters(
+        value.registers, Endian.Big, wordorder=Endian.Big
+    )
 
     assert decoder.decode_64bit_int() == random_value
     modbusClient.close()
     mqttClient.disconnect()
 
+
 @pytest.mark.end_to_end
 def test_able_to_send_uint64():
     random_value = random.randint(0, 100)
-    message_dictorionary = {
-        "action": "testUInt64Register",
-        "value": random_value
-    }
+    message_dictorionary = {"action": "testUInt64Register", "value": random_value}
 
     message = json.dumps(message_dictorionary)
 
@@ -178,7 +168,6 @@ def test_able_to_send_uint64():
         "localhost",
         5020,
     )
-
 
     modbusClient.connect()
     mqttClient.connect("localhost", 1883, 60)
@@ -189,7 +178,9 @@ def test_able_to_send_uint64():
 
     value = modbusClient.read_holding_registers(30, 8, 1)
 
-    decoder = BinaryPayloadDecoder.fromRegisters(value.registers, Endian.Big, wordorder=Endian.Big)
+    decoder = BinaryPayloadDecoder.fromRegisters(
+        value.registers, Endian.Big, wordorder=Endian.Big
+    )
 
     assert decoder.decode_64bit_uint() == random_value
     modbusClient.close()
