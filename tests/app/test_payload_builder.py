a2f23d314e782b805c11d764557045b41db4c8d8
pieper126 <stijnbijnen@hotmail.com>
2023-06-06 21:40:09 +0200

    Add more unit tests to payload builder (#27)
    * Add more unit tests to payload builder
    
    * Add three extras cases when things are set to none
    
    ---------
    
    Co-authored-by: Stijn Bijnen <stijn.bijnen@evergen.energy>

--- tests/app/test_payload_builder.py
+++ tests/app/test_payload_builder.py
@@ -35,12 +35,122 @@ def test_able_to_create_float64():
     assert payload is not None
 
 
+def test_able_to_create_unit64():
+    builder = PayloadBuilder()
+
+    builder.set_data_type("UINT64")
+    builder.set_value(10)
+    builder.set_memory_order(MemoryOrder("AB"))
+    payload = builder.build()
+    assert payload is not None
+    assert len(payload) == 4
+    assert payload == [0, 0, 0, 10]
+
+
+def test_able_to_create_uint32():
+    builder = PayloadBuilder()
+
+    builder.set_data_type("UINT32")
+    builder.set_value(10)
+    builder.set_memory_order(MemoryOrder("AB"))
+    payload = builder.build()
+    assert payload is not None
+    assert len(payload) == 2
+    assert payload == [0, 10]
+
+
+def test_able_to_create_uint8():
+    builder = PayloadBuilder()
+
+    builder.set_data_type("UINT8")
+    builder.set_value(0)
+    builder.set_memory_order(MemoryOrder("AB"))
+    payload = builder.build()
+    assert payload is not None
+    assert len(payload) == 1
+    assert payload == [0]
+
+
+def test_able_to_create_uint16():
+    builder = PayloadBuilder()
+
+    builder.set_data_type("UINT16")
+    builder.set_value(10)
+    builder.set_memory_order(MemoryOrder("AB"))
+    payload = builder.build()
+    assert payload is not None
+    assert len(payload) == 1
+    assert payload == [10]
+
+
+def test_able_to_create_int8():
+    builder = PayloadBuilder()
+
+    builder.set_data_type("INT8")
+    builder.set_value(0)
+    builder.set_memory_order(MemoryOrder("AB"))
+    payload = builder.build()
+    assert payload is not None
+    assert len(payload) == 1
+    assert payload == [0]
+
+
+def test_able_to_create_int16():
+    builder = PayloadBuilder()
+
+    builder.set_data_type("INT16")
+    builder.set_value(5)
+    builder.set_memory_order(MemoryOrder("AB"))
+    payload = builder.build()
+    assert payload is not None
+    assert len(payload) == 1
+    assert payload == [5]
+
+
+def test_able_to_create_int32():
+    builder = PayloadBuilder()
+
+    builder.set_data_type("INT32")
+    builder.set_value(10)
+    builder.set_memory_order(MemoryOrder("AB"))
+    payload = builder.build()
+    assert payload is not None
+    assert len(payload) == 2
+    assert payload == [0, 10]
+
+
+def test_able_to_create_int64():
+    builder = PayloadBuilder()
+
+    builder.set_data_type("INT64")
+    builder.set_value(10)
+    builder.set_memory_order(MemoryOrder("AB"))
+    payload = builder.build()
+    assert payload is not None
+    assert len(payload) == 4
+    assert payload == [0, 0, 0, 10]
+
+
+def test_able_to_create_string():
+    builder = PayloadBuilder()
+
+    builder.set_data_type("STRING")
+    builder.set_value("testing")
+    builder.set_memory_order(MemoryOrder("AB"))
+    payload = builder.build()
+    assert payload is not None
+
+
 def test_if_no_memory_order_been_set():
     builder = PayloadBuilder()
 
     builder.set_data_type("FLOAT64-IEEE")
     builder.set_value(20)
 
+    with pytest.raises(AttributeError):
+        builder.build()
+
+    builder.set_memory_order(None)
     with pytest.raises(AttributeError):
         builder.build()
 
@@ -54,6 +164,10 @@ def test_if_no_data_type_has_been_set():
     with pytest.raises(AttributeError):
         builder.build()
 
+    builder.set_data_type(None)
+    with pytest.raises(AttributeError):
+        builder.build()
+
 
 def test_if_no_value_has_been_set():
     builder = PayloadBuilder()
@@ -62,4 +176,19 @@ def test_if_no_value_has_been_set():
     builder.set_data_type("FLOAT64-IEEE")
 
     with pytest.raises(AttributeError):
+        builder.build()
+
+    builder.set_value(None)
+    with pytest.raises(AttributeError):
+        builder.build()
+
+
+def test_if_unknown_data_type_is_given():
+    builder = PayloadBuilder()
+
+    builder.set_memory_order(MemoryOrder("AB"))
+    builder.set_data_type("unknown data type")
+    builder.set_value(1)
+
+    with pytest.raises(RuntimeError):
         builder.build()
