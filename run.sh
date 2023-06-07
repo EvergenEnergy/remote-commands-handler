6e3aa2ce32575c854dfd760f417bcf457ff43896
Sebastian Machuca <sebastian.machuca@evergen.energy>
2023-06-07 14:54:58 +1000

    Run ShellCheck on each PR containing changes with shell scripts

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ if [ -z "$CONFIGURATION_PATH" ]
 then
     python3 main.py "$@"
 else
-    python3 main.py --configuration_path $CONFIGURATION_PATH "$@"
+    python3 main.py --configuration_path "$CONFIGURATION_PATH" "$@"
 fi
 
