7b4e824d48c8aa82b8b83ab9ce37cad27bb76298
pieper126 <stijnbijnen@hotmail.com>
2023-06-07 04:39:01 +0200

    Initial setup for python end-to-end tests (#20)
    * initial setup for python integration tests
    
    * rename integration tests to end-to-end tests
    
    * Add pytest mark end to end to end to end tests
    
    * Add end to end ci yaml
    
    * remove space from modubs config.json
    
    * fix coniguration yaml in tests
    
    * ran yamlfix to make it adhere to our yaml linter
    
    * add extra newline
    
    * ran black formatter
    
    * remove coverage step from end-to-end because it is not relevant
    
    * fix mistake in test_able_to_update_coil
    
    * Add markers to pytest.ini to stop warnings
    
    * update coverage run to check only for source files
    
    * Add new line to pytest.ini
    
    * Clean up tests
    
    * improve test approach for random
    
    * fix linting
    
    ---------
    
    Co-authored-by: Stijn Bijnen <stijn.bijnen@evergen.energy>
    Co-authored-by: Sebastian Machuca <sebastian.machuca@evergen.energy>

---
+++ tests/end-to-end/test_able_to_sent_to_commands.py
@@ -0,0 +1,144 @@
+import time
+import json
+import random
+
+import pytest
+
+import paho.mqtt.client as mqtt
+from pymodbus.client import ModbusTcpClient
+from pymodbus.payload import BinaryPayloadDecoder
+from pymodbus.constants import Endian
+
+
+@pytest.fixture(scope="module")
+def mqtt_client():
+    client = mqtt.Client()
+    client.connect("localhost", 1883, 60)
+    yield client  # provide the client for tests
+    client.disconnect()  # disconnect after tests
+
+
+@pytest.fixture(scope="module")
+def modbus_client():
+    client = ModbusTcpClient("localhost", 5020)
+    client.connect()
+    yield client  # provide the client for tests
+    client.close()  # close after tests
+
+
+@pytest.mark.end_to_end
+@pytest.mark.parametrize("coil_value", [False, True])
+def test_able_to_update_coil(
+    mqtt_client: mqtt.Client, modbus_client: ModbusTcpClient, coil_value: bool
+):
+    message_dictionary = {"action": "testCoil", "value": coil_value}
+    message = json.dumps(message_dictionary)
+
+    mqtt_client.publish("commands/test", message)
+
+    time.sleep(1)
+
+    value = modbus_client.read_coils(504, 1, 1)
+
+    assert value.bits[0] is coil_value
+
+
+@pytest.mark.end_to_end
+def test_able_to_send_integer16(
+    mqtt_client: mqtt.Client, modbus_client: ModbusTcpClient
+):
+    random_value = random.randint(0, 30)
+    message_dictorionary = {"action": "testRegister", "value": random_value}
+
+    message = json.dumps(message_dictorionary)
+
+    mqtt_client.publish("commands/test", message)
+
+    time.sleep(1)
+
+    value = modbus_client.read_holding_registers(1, 1, 1)
+
+    assert value.registers[0] == random_value
+
+
+@pytest.mark.end_to_end
+def test_able_to_send_float32(mqtt_client: mqtt.Client, modbus_client: ModbusTcpClient):
+    random_value = random.uniform(0, 30)
+    message_dictionary = {"action": "testFloatRegister", "value": random_value}
+
+    message = json.dumps(message_dictionary)
+
+    mqtt_client.publish("commands/test", message)
+
+    time.sleep(1)
+
+    value = modbus_client.read_holding_registers(2, 4, 1)
+
+    decoder = BinaryPayloadDecoder.fromRegisters(
+        value.registers, Endian.Big, wordorder=Endian.Big
+    )
+
+    assert round(decoder.decode_32bit_float(), 4) == round(random_value, 4)
+
+
+@pytest.mark.end_to_end
+def test_able_to_send_command_with_float64(
+    mqtt_client: mqtt.Client, modbus_client: ModbusTcpClient
+):
+    random_value = random.uniform(0, 30)
+    message_dictionary = {"action": "testFloat64Register", "value": random_value}
+
+    message = json.dumps(message_dictionary)
+
+    mqtt_client.publish("commands/test", message)
+
+    time.sleep(1)
+
+    value = modbus_client.read_holding_registers(3, 8, 1)
+
+    decoder = BinaryPayloadDecoder.fromRegisters(
+        value.registers, Endian.Big, wordorder=Endian.Big
+    )
+    assert decoder.decode_64bit_float() == random_value
+
+
+@pytest.mark.end_to_end
+def test_able_to_send_integer64(
+    mqtt_client: mqtt.Client, modbus_client: ModbusTcpClient
+):
+    random_value = random.randint(0, 100)
+    message_dictorionary = {"action": "testInt64Register", "value": random_value}
+
+    message = json.dumps(message_dictorionary)
+
+    mqtt_client.publish("commands/test", message)
+
+    time.sleep(1)
+
+    value = modbus_client.read_holding_registers(20, 8, 1)
+
+    decoder = BinaryPayloadDecoder.fromRegisters(
+        value.registers, Endian.Big, wordorder=Endian.Big
+    )
+
+    assert decoder.decode_64bit_int() == random_value
+
+
+@pytest.mark.end_to_end
+def test_able_to_send_uint64(mqtt_client: mqtt.Client, modbus_client: ModbusTcpClient):
+    random_value = random.randint(0, 100)
+    message_dictorionary = {"action": "testUInt64Register", "value": random_value}
+
+    message = json.dumps(message_dictorionary)
+
+    mqtt_client.publish("commands/test", message)
+
+    time.sleep(1)
+
+    value = modbus_client.read_holding_registers(30, 8, 1)
+
+    decoder = BinaryPayloadDecoder.fromRegisters(
+        value.registers, Endian.Big, wordorder=Endian.Big
+    )
+
+    assert decoder.decode_64bit_uint() == random_value
